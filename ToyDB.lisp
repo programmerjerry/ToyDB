f978320d6a42420b7bb887f6b670930ca9d59cfd
Jerry <me@programmerjerry.com>
2019-04-19 21:33:56 +0800

    修改存储结构；完成update函数

--- ToyDB.lisp
+++ ToyDB.lisp
@@ -48,20 +48,30 @@
     (setf *db* nil)
     t))
 
+(defun update (table-name id student-name)
+  (declare (ignore table-name))
+  (let ((tmp-db nil))
+    (dolist (e *db*)
+      (progn
+        (if (= (first e) id)
+            (setf tmp-db (append (list (list id student-name)) tmp-db))
+          (setf tmp-db (append (list e) tmp-db)))))
+    (setf *db* tmp-db)))
+
 
 
 (fiveam:def-fixture fix-update ()
   (progn
-    (setf *db*
-          '(((ID 44) (STUDENT_NAME "Jerry")))) ;直接给数据库赋值
+    (setf *db* '((44 "Jerry")(55 "Tom"))) ;直接给数据库赋值
     (&body)
     (setf *db* nil)))
 
 (fiveam:test update-test
   (fiveam:with-fixture fix-update ()
     (progn
-      (update "student" 44 "TOM")
-      (fiveam:is-true (equal (car (cdadar *db*)) "Jerry")))))
+      (update "student" 55 "CAT")
+      (fiveam:is-true (equal  (cadar *db*) "CAT")))));lisp的append存的内容与其他编程语言里面是相反的，这里append后*db*里面存到是((55 "Tom") (44 "Jerry"))
+
     
 
 
